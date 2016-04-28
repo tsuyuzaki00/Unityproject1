8634f1380ea748a3c6a991c8be8eb57877a5650b
kuroma <mugen.tsuyuzaki@gmail.com>
2016-04-28 13:53:41 +0900

    4/28 アナログスティックに倒した方向に移動する

--- Unity/professionalProject1/Assets/Player.cs
+++ Unity/professionalProject1/Assets/Player.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	public bool s = false;
+	public bool Inputmove = true;
 
 	// Use this for initialization
 	void Start () {
@@ -12,23 +12,26 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Inputmove = false;
 		if(Input.GetAxis("Vertical") < -0.2){
 			transform.Translate(0,0,0.1f,Space.World);
-			s = true;
+			Inputmove = true;
 		}else if(Input.GetAxis("Vertical") > 0.2){
 			transform.Translate(0,0,-0.1f,Space.World);
-			s = true;
+			Inputmove = true;
 		}if(Input.GetAxis("Horizontal") < -0.2){
 			transform.Translate(-0.1f,0,0,Space.World);
-			s = true;
+			Inputmove = true;
 		}else if(Input.GetAxis("Horizontal") > 0.2){
 			transform.Translate(0.1f,0,0,Space.World);
-			s = true;
-		}
-		s = true;
+			Inputmove = true;
+		}if(Inputmove){
 		transform.LookAt(new Vector3(Input.GetAxis("Horizontal"),0,-Input.GetAxis("Vertical"))+(transform.position));
-		Debug.Log(Input.GetAxis("Vertical"));
-		Debug.Log(Input.GetAxis("Horizontal"));
+		}
 		Debug.Log(transform.forward);
 	}
+
+//	void OnCollisionEnter() {
+//		GetComponent<Rigidbody>(0,10,0);GetComponent;
+//	}
 }
