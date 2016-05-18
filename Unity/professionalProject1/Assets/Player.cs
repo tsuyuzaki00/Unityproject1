25dcd93c1b33b6dfb77f4b7d4ecbdd5a7eab9cb8
kuroma <mugen.tsuyuzaki@gmail.com>
2016-05-19 00:06:21 +0900

    仮のプレイヤーモデルと仮のアニメーションを追加

--- Unity/professionalProject1/Assets/Player.cs
+++ Unity/professionalProject1/Assets/Player.cs
@@ -4,32 +4,42 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
 	public bool Inputmove = true;
+    public Animator HumanAnimator;
 
-	// Use this for initialization
-	void Start () {
-	
+    // Use this for initialization
+    void Start () {
+        HumanAnimator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        bool fire = Input.GetButtonDown("Fire1");
+
+        if (fire){
+            HumanAnimator.Play("Attack1");
+            Debug.Log("1ボタン");
+        }
 		Inputmove = false;
-		if(Input.GetAxis("Vertical") < -0.2){
+		if(v < -0.2){
 			transform.Translate(0,0,0.1f,Space.World);
 			Inputmove = true;
-		}else if(Input.GetAxis("Vertical") > 0.2){
+		}else if(v > 0.2){
 			transform.Translate(0,0,-0.1f,Space.World);
 			Inputmove = true;
-		}if(Input.GetAxis("Horizontal") < -0.2){
+		}if(h < -0.2){
 			transform.Translate(-0.1f,0,0,Space.World);
 			Inputmove = true;
-		}else if(Input.GetAxis("Horizontal") > 0.2){
+		}else if(h > 0.2){
 			transform.Translate(0.1f,0,0,Space.World);
 			Inputmove = true;
 		}if(Inputmove){
 		transform.LookAt(new Vector3(Input.GetAxis("Horizontal"),0,-Input.GetAxis("Vertical"))+(transform.position));
 		}
-		Debug.Log(transform.forward);
-	}
+        //Debug.Log(transform.forward);
+
+    }
 
 //	void OnCollisionEnter() {
 //		GetComponent<Rigidbody>(0,10,0);GetComponent;
